7a3f850bf2d26df64d373784126238dac2133cb7
Kiyotaka <t10kiyotaka@gmail.com>
2020-09-06 14:25:19 +0900

    Add row to show phone and email

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,67 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'Kiyotaka Tomii',
-                style:
-                    TextStyle(color: Colors.white, fontSize: 40.0, fontWeight: FontWeight.bold, fontFamily: 'Pacifico'),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
               ),
               Text(
                 'Web developer',
-                style: TextStyle(color: Colors.white, fontSize: 20.0, fontFamily: 'RobotoMono'),
-              )
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontFamily: 'RobotoMono',
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+81 123 456 789',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'kiyotaka@email.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
